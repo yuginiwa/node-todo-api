3ce89d4b9829c87863b401caa406fbd5db44fabe
Carl Vincent Baydo <cvbaydo@gmail.com>
2016-12-27 14:01:39 +0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const {ObjectID} = require('mongodb');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
@@ -26,6 +27,23 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }, catch((e) => {
+    res.status(404).send();
   });
 });
 
