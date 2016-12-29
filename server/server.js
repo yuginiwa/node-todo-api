0c3ca81b87c62678e806bf0ad10c359ad4331cad
Carl Vincent Baydo <cvbaydo@gmail.com>
2016-12-29 16:15:22 +0800

    Add DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
-const port = PROCESS.ENV.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -45,7 +45,25 @@ app.get('/todos/:id', (req, res) => {
     }
     res.send({todo});
   }).catch((e) => {
-    res.status(404).send();
+    res.status(400).send();
+  });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
