09f04d3f9ba9faf44a3032e0965601024a41c78d
Carl Vincent Baydo <cvbaydo@gmail.com>
2017-01-18 10:05:55 +0800

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -110,6 +110,18 @@ app.post('/users', (req, res) => {
   });
 });
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ["email", "password"]);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
