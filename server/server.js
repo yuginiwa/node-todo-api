57276dc2c1a95d8fbcaa02aee2e13dd821e2bdd9
Carl Vincent Baydo <cvbaydo@gmail.com>
2017-01-30 15:55:41 +0800

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -127,6 +127,14 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is up in ${port}`);
 });
